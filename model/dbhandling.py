3d887f6b9c5c3b1f29c70e85d3d33863b462961c
Eduard Buna <eduardbuna@yahoo.com>
2023-10-25 11:41:53 -0400

    Schema edits and getting projs and cols

--- model/dbhandling.py
+++ model/dbhandling.py
@@ -2,3 +2,39 @@ import pyodbc
 import hashlib
 import re
 import datetime
+from connection import DBCursor
+
+# Returns columns of accessed table in a nicer list of string format with added
+# spaces instead of using the keys in entry dicts
+def getColumns(table):
+    query = 'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = ?'
+    with DBCursor() as cursor:
+        cols = cursor.makeQuery(query, table)
+    out = []
+    for col in cols:
+        colName = re.sub(r'(\w)([A-Z])', r'\1 \2', col[0]).replace('_', '')
+        out.append(colName)
+    return out
+
+# Get all Projects defined in the DB
+def getAllProjects():
+    query = 'select * from Projects'
+    with DBCursor() as cursor:
+        projs = makeEntryDicts(cursor.makeQuery(query), 'Projects')
+    out = []
+    for proj in projs:
+        proj["Date Created"] = str(proj["Date Created"].strftime("%a, %d %b %Y"))#datetime.datetime.strptime(proj["Date Created"], "%a, %d %b %Y, %H:%M:%S %Z").date()
+        out.append(proj)
+    return out
+
+# Util function to "prettify" all entries, creating dicts for json transfer
+# and using formatted column names
+def makeEntryDicts(entries, table):
+    newEntries = []
+    cols = getColumns(table)
+    for entry in entries:
+        entryDict = {}
+        for index, col in enumerate(cols):
+            entryDict[col] = entry[index]
+        newEntries.append(entryDict.copy())
+    return newEntries
