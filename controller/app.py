1c26a3288fe5b1604c94364186029c3b0995a5dd
Eduard Buna <eduardbuna@yahoo.com>
2023-10-25 13:40:49 -0400

    Endpoints for getting project items and projects

--- controller/app.py
+++ controller/app.py
@@ -18,6 +18,69 @@ CORS(app)
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret'
 app.config['SECRET_KEY'] = SECRET_KEY
 
+# Endpoint for getting 
+@app.route('/get_projects', methods=['GET'])
+def get_projects():
+    try:
+        try:
+            projects = db.getAllProjects()
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'error': 'Get Projects Error',
+                'message':'Error fetching all Projects: ' + str(e)}), 500  
+
+        try:
+            projectCols = db.getColumns('Projects')
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'error': 'Get Columns Error',
+                'message':'Error fetching columns of Project_Items' + ': ' + str(e)}), 500
+
+        response = jsonify({
+            'entries': projects, 
+            'projectNumber': 'All Projects', 
+            'name': 'All Projects', 
+            'columns': projectCols, 
+            'success': True})
+        return response, 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': str(e), 'message': str(e)}), 500
+
+# Endpoint fro returning All Items seen by the db
+@app.route('/get_items', methods=['GET'])
+def get_items():
+    try:
+        try:
+            items = db.getAllItems()
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'error': 'Get All Items Error', 
+                'message':'Error fetching all items: ' + str(e)}), 500
+        
+        try:
+            allItemsCols = db.getColumns('All_Items')
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'error': 'Get Columns Error', 
+                'message':'Error fetching columns of All Items: ' + str(e)}), 500
+
+        # Creates json with output data
+        response = jsonify({
+            'entries': items,
+            'projectNumber': 'All Items',
+            'name': 'All Items',
+            'columns': allItemsCols,
+            'success': True})
+        return response, 200
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': str(e), 'message': str(e)}), 500
+
 # Endpoint for getting all items in a project
 @app.route('/get_project_items', methods=['GET'])
 def get_project_items():
@@ -28,28 +91,41 @@ def get_project_items():
             projects = db.getProject(projectNumber)
         except Exception as e:
             print(e)
-            return jsonify({'error': 'Get Project Error', 'message':'Error fetching project ' + str(projectNumber) + ': ' + str(e)}), 500
+            return jsonify({
+                'error': 'Get Project Error', 
+                'message':'Error fetching project ' + str(projectNumber) + ': ' + str(e)}), 500
         if len(projects) == 0:
-            return jsonify({'error': 'JSON Error', 'message':'No project with name: ' + str(projectNumber) + ' found'}), 400
+            return jsonify({
+                'error': 'JSON Error', 
+                'message':'No project with name: ' + str(projectNumber) + ' found'}), 400
         
         project = projects[0]
         try:
             projectData = db.getProjectItems(projectNumber)
         except Exception as e:
             print(e)
-            return jsonify({'error': 'Get Project Items Error', 'message':'Error fetching project items in project ' + str(projectNumber) + ': ' + str(e)}), 500
+            return jsonify({
+                'error': 'Get Project Items Error', 
+                'message':'Error fetching project items in project ' + str(projectNumber) + ': ' + str(e)}), 500
         
         project = projects[0]
         try:
             projectCols = db.getColumns('Project_Items')
         except Exception as e:
             print(e)
-            return jsonify({'error': 'Get Columns Error', 'message':'Error fetching columns of  ' + str('Project_Items') + ': ' + str(e)}), 500
+            return jsonify({
+                'error': 'Get Columns Error', 
+                'message':'Error fetching columns of  ' + str('Project_Items') + ': ' + str(e)}), 500
 
         projectName = project['Project Name']
 
         # Creates json with output data
-        response = jsonify({'entries': projectData, 'projectNumber': projectNumber, 'name': projectName, 'columns': projectCols, 'success': True})
+        response = jsonify({
+            'entries': projectData,
+            'projectNumber': projectNumber,
+            'name': projectName,
+            'columns': projectCols,
+            'success': True})
         return response, 200
     except Exception as e:
         print(str(e))
